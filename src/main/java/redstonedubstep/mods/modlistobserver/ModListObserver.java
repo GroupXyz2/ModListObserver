9adc094215d3c23a582072a4c2a4dd0d455ffd81
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2025-04-24 14:20:02 +0200

    v1.0.4 - Added config to allow broadcasting mod lists to admins on join
    Closes #1

--- src/main/java/redstonedubstep/mods/modlistobserver/ModListObserver.java
+++ src/main/java/redstonedubstep/mods/modlistobserver/ModListObserver.java
@@ -2,6 +2,7 @@ package redstonedubstep.mods.modlistobserver;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,6 +11,11 @@ import org.apache.logging.log4j.Logger;
 
 import com.mojang.authlib.GameProfile;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.IExtensionPoint;
@@ -17,6 +23,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.network.NetworkConstants;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 @Mod(ModListObserver.MODID)
 public class ModListObserver {
@@ -51,7 +58,32 @@ public class ModListObserver {
 		ALL_SESSION_MODS.putIfAbsent(player, modList);
 		ALL_SESSION_MODS.get(player).addAll(modList);
 
+		String messageString = String.format("Player %s connected with mods %s", player.getName(), String.join(", ", modList));
+
 		if (ModListObserverConfig.CONFIG.logJoiningModList.get())
-			LOGGER.info("Player " + player.getName() + " connected with mods " + String.join(", ", modList));
+			LOGGER.info(messageString);
+
+		if (ModListObserverConfig.CONFIG.broadcastModListOnJoin.get())
+			broadcastModList(messageString, player);
+
+	}
+
+	public static void broadcastModList(String messageString, GameProfile player) {
+		MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+
+		if (server != null) {
+			List<ServerPlayer> playerList = server.getPlayerList().getPlayers();
+			int joiningPlayerOpLevel = server.getProfilePermissions(player);
+
+			if (joiningPlayerOpLevel <= ModListObserverConfig.CONFIG.broadcastPermissionLevel.get()) {
+				MutableComponent prefix = Component.literal("[").append(Component.literal("ModListObserver").withStyle(ChatFormatting.GRAY)).append("] ");
+				Component message = prefix.append(messageString);
+
+				for (ServerPlayer otherPlayer : playerList) {
+					if (otherPlayer.hasPermissions(ModListObserverConfig.CONFIG.receiveBroadcastPermissionLevel.get()))
+						otherPlayer.sendSystemMessage(message);
+				}
+			}
+		}
 	}
 }
--- src/main/java/redstonedubstep/mods/modlistobserver/ModListObserverConfig.java
+++ src/main/java/redstonedubstep/mods/modlistobserver/ModListObserverConfig.java
@@ -24,12 +24,15 @@ public class ModListObserverConfig {
 		public BooleanValue logJoiningModList;
 		public BooleanValue logServerMods;
 		public BooleanValue allowlistEnabled;
+		public BooleanValue broadcastModListOnJoin;
 		public IntValue modlistCommandPermissionLevel;
+		public IntValue broadcastPermissionLevel;
+		public IntValue receiveBroadcastPermissionLevel;
 		public ForgeConfigSpec.ConfigValue<List<String>> allowlist;
 
 		Config(ForgeConfigSpec.Builder builder) {
 			logJoiningModList = builder
-					.comment(" --- ModListObserver Config File --- ", "Should the current mod list of joining players be logged?")
+					.comment(" --- ModListObserver Config File --- ", "Should the current mod list of joining players be printed in the server console?")
 					.define("logJoiningModList", true);
 			logServerMods = builder
 					.comment("Should mods of a joining player that are also present on the server be logged?")
@@ -37,11 +40,20 @@ public class ModListObserverConfig {
 			allowlistEnabled = builder
 					.comment("Should only mods that are not allowlisted (via the \"allowlist\" config) be recorded and logged?")
 					.define("allowlistEnabled", false);
+			broadcastModListOnJoin = builder
+					.comment("Should the current mod list of a joining player be broadcast in chat to online players?")
+					.define("broadcastModListOnJoin", false);
 			modlistCommandPermissionLevel = builder
 					.comment("What op permission level should be the requirement for being able to execute /modlist?")
 					.defineInRange("modlistCommandPermissionLevel", 3, 0, 4);
+			broadcastPermissionLevel = builder
+					.comment("What's the maximum (inclusive) op permission level of joining players that should have their mod list broadcast in chat to eligible players?", "For example: With a value of 0, the mod list of joining non-admins will be broadcast, while the mod list of joining admins (op permission level > 0) will not be broadcast.")
+					.defineInRange("broadcastPermissionLevel", 0, 0, 4);
+			receiveBroadcastPermissionLevel = builder
+					.comment("What op permission level should be the requirement for receiving in-game broadcasts of mod lists of joining players?")
+					.defineInRange("receiveBroadcastPermissionLevel", 1, 0, 4);
 			allowlist = builder
-					.comment("A comma-separated list of mod ids that are allowed by the server and thus shouldn't be recorded and logged")
+					.comment("A comma-separated list of mod ids that are allowed by the server and thus shouldn't be recorded and logged. Example: [\"jei\", \"neat\"]")
 					.define("allowlist", new ArrayList<>());
 		}
 	}
