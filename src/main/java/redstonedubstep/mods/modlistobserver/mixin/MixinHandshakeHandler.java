e4cc22543219806bb496bb34998b488b2b22fe19
GroupXyz <122353547+GroupXyz2@users.noreply.github.com>
2025-04-26 23:06:00 +0000

    Update MixinHandshakeHandler.java

--- src/main/java/redstonedubstep/mods/modlistobserver/mixin/MixinHandshakeHandler.java
+++ src/main/java/redstonedubstep/mods/modlistobserver/mixin/MixinHandshakeHandler.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.function.Supplier;
 
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -41,6 +42,24 @@ public abstract class MixinHandshakeHandler {
 				profile = new GameProfile(UUIDUtil.getOrCreatePlayerUUID(profile), profile.getName());
 
 			ModListObserver.updateModListOnJoin(new HashSet<>(clientMods), profile);
+		}
+
+		if (!ModListObserverConfig.CONFIG.whitelist.get().isEmpty()) {
+			List<String> whitelist = ModListObserverConfig.CONFIG.whitelist.get();
+			List<String> disallowedMods = clientMods.stream().filter(mod -> !whitelist.contains(mod)).toList();
+
+			if (!disallowedMods.isEmpty()) {
+				packetListener.disconnect(Component.literal("This server is using a mod whitelist. You are using disallowed mods: " + String.join(", ", disallowedMods)));
+            }
+		}
+
+		if (!ModListObserverConfig.CONFIG.blacklist.get().isEmpty()) {
+			List<String> blacklist = ModListObserverConfig.CONFIG.blacklist.get();
+			List<String> disallowedMods = clientMods.stream().filter(blacklist::contains).toList();
+
+			if (!disallowedMods.isEmpty()) {
+				packetListener.disconnect(Component.literal("This server is using a mod blacklist. You are using disallowed mods: " + String.join(", ", disallowedMods)));
+			}
 		}
 	}
 }
